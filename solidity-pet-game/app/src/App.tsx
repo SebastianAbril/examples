2e6abbfadad57d9badd8b078ae9561e9ada1da7e
juandaabril <juandaabril@gmail.com>
2021-09-30 06:24:19 -0500

    create-react-app

---
+++ solidity-pet-game/app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Album from './Album';
+
+function App() {
+  return (
+    <Album />
+  );
+}
+
+export default App;
